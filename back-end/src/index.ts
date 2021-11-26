63f03153fe7611dac53419d3ef1a006bc64e2e88
Lucas Almeida <almeidam.lucas@outlook.com>
2021-11-26 16:52:51 -0300

    Tslint configurado

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -1,20 +1,21 @@
 require('dotenv').config();
-const express = require('express')
-const mongoose = require('mongoose')
-const cors = require('cors')
-const routes = require('./routes/watch-list/index')
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const routes = require('./routes/watch-list/index');
 
 const port = 3333;
-const app = express()
+const app = express();
 
 mongoose.connect(process.env.MONGO_CREDENTIALS, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
 
-app.use(cors())
-app.use(express.json())
-app.use(routes)
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}!`);
